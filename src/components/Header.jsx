364e306c3a3ef3f94bd98f36f6854a349b29430d
David Nnamdi <95830108+Dahvidh@users.noreply.github.com>
2023-11-26 16:50:20 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,67 +1,72 @@
-import React from 'react'
+import React from 'react';
+import 'animate.css';
+//import { BxFacebook, BxTwitter, BxInstagram, BxGithub, BxYoutube } from 'boxicons';
 
 function Header() {
   return (
     <header className="header">
-    <nav className="nav container flex">
-      <a href="index.html" className="logo-content flex">
-        <img src="images/LOGO.svg" />
-        {/*<span class="logo-text">DahVidh</span>*/}
-      </a>
-      <div className="menu-content">
-        <ul className="menu-list flex">
-          <li>
-            <a href="#" className="nav-link">
-              About Us
+      <nav className="nav container flex animate__animated animate__backInLeft animate__delay-5s">
+        <a href="index.html" className="logo-content flex">
+          <img src="images/LOGO.svg" alt="" />
+          {/*<span className="logo-text">DahVidh</span>*/}
+        </a>
+        <div className="menu-content ">
+          <ul className="menu-list flex">
+            <li>
+              <a href="#" className="nav-link">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Our Cocoa
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                How It Works
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a className="button3" href="#">
+                Login{" "}
+              </a>
+            </li>
+            <li>
+              <a className="button" href="#">
+                Register{" "}
+              </a>
+            </li>
+          </ul>
+          <div className="media-icons flex">
+            <a href="https://www.facebook.com">
+              {/*<BxFacebook />*/} 
             </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link">
-              Our Cocoa
+            <a href="https://twitter.com/i/flow/login">
+             {/*<BxTwitter/> */} 
             </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link">
-              How It Works
+            <a href="https://www.instagram.com/accounts/login">
+              {/* <BxInstagram />*/}
             </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link">
-              Contact Us
+            <a href="https://github.com/login">
+             {/* <BxGithub />*/} 
             </a>
-          </li>
-        </ul>
-        <div className="media-icons flex">
-          <a href="https://www.facebook.com">
-            <i className="bx bxl-facebook" />
-          </a>
-          <a href="https://twitter.com/i/flow/login">
-            <i className="bx bxl-twitter" />
-          </a>
-          <a href="https://www.instagram.com/accounts/login">
-            <i className="bx bxl-instagram-alt" />
-          </a>
-          <a href="https://github.com/login">
-            <i className="bx bxl-github" />
-          </a>
-          <a href="https://www.youtube.com/login">
-            <i className="bx bxl-youtube" />
-          </a>
+            <a href="https://www.youtube.com/login">
+              {/*<BxYoutube/> */}
+            </a>
+          </div>
+          <i className="bx bx-x navClose-btn" />
         </div>
-        <i className="bx bx-x navClose-btn" />
-      </div>
-      <div className="contact-content flex">
-        <button className="button3" href="#">
-          Login{" "}
-        </button>
-        <button className="button" href="#">
-          Register{" "}
-        </button>
-      </div>
-      <i className="bx bx-menu navOpen-btn" />
-    </nav>
-  </header>
-  )
+
+        <i className="bx bx-menu navOpen-btn" />
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
