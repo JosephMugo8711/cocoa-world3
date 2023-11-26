092bf832569fe438e83b8b085cb9c32a4c64df25
David Nnamdi <95830108+Dahvidh@users.noreply.github.com>
2023-11-26 17:06:39 +0100

    Update Cocoa.jsx

--- src/components/Cocoa.jsx
+++ src/components/Cocoa.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import 'animate.css';
 
 function Cocoa() {
   return (
     <section className="section brand">
-      <div className="brand-container container">
+      <div className="brand-container container animate__animated animate__backInLeft animate__delay-5s">
         <h2
           style={{
             alignItems: "center",
@@ -16,33 +17,33 @@ function Cocoa() {
         <div className="brand-images">
           <div className="brand-image">
             <img src="images/1 (1).png" alt="" className="brand-img" />
-            {/*<button
+            <button
               className="button2"
               href="#"
               style={{ justifyContent: "center", display: "flex" }}
             >
               See More{" "}
-            </button>*/}
+            </button>
           </div>
           <div className="brand-image">
             <img src="images/2 (1).png" alt="" className="brand-img" />
-            {/*<button
+            <button
               className="button2"
               href="#"
               style={{ justifyContent: "center", display: "flex" }}
             >
               See More{" "}
-            </button>*/}
+            </button>
           </div>
           <div className="brand-image">
             <img src="images/3 (1).png" alt="" className="brand-img" />
-           {/*<button
+           <button
               className="button2"
               href="#"
               style={{ justifyContent: "center", display: "flex" }}
             >
               See More{" "}
-            </button>*/}
+            </button>
           </div>
         </div>
       </div>
