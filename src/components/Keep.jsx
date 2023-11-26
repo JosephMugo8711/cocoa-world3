86a5844db3e2643615e72ccd130f1b666b19ca81
David Nnamdi <95830108+Dahvidh@users.noreply.github.com>
2023-11-26 17:12:37 +0100

    Update Keep.jsx

--- src/components/Keep.jsx
+++ src/components/Keep.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import 'animate.css';
 
-function Keep() {
+function Keep() { 
   return (
     <section className="section brand">
-    <div className="brand-container container">
+    <div className="brand-container container animate__animated animate__backInLeft animate__delay-5s, animate__animate__backOutRight animate__delay-2s">
       <h2
         style={{
           alignItems: "center",
@@ -16,21 +17,134 @@ function Keep() {
       <div className="brand-images">
         <div className="brand-image">
           <img src="images/Blog 1.png" alt="" className="brand-img" />
+          <button
+            className="btn2"
+            href="#"
+          >
+            Read more{" "}
+          </button> 
         </div>
         <div className="brand-image">
           <img src="images/Blog 2.png" alt="" className="brand-img" />
+          <button
+            className="btn2"
+            href="#"
+          >
+            Read more{" "}
+          </button> 
         </div>
         <div className="brand-image">
           <img src="images/Blog 3.png" alt="" className="brand-img" />
-        </div>
-        <button
+          <button
             className="btn2"
             href="#"
-            style={{ display: "flex", top: "29%",
-            left: "50%" }}
           >
             Read more{" "}
-          </button>
+          </button> 
+        </div>
+       
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Keepimport React from 'react'
+import 'animate.css';
+
+function Keep() { 
+  return (
+    <section className="section brand">
+    <div className="brand-container container animate__animated animate__backInLeft animate__delay-5s, animate__animate__backOutRight animate__delay-2s">
+      <h2
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex"
+        }}
+      >
+        Keep Up With Us
+      </h2>
+      <div className="brand-images">
+        <div className="brand-image">
+          <img src="images/Blog 1.png" alt="" className="brand-img" />
+          <button
+            className="btn2"
+            href="#"
+          >
+            Read more{" "}
+          </button> 
+        </div>
+        <div className="brand-image">
+          <img src="images/Blog 2.png" alt="" className="brand-img" />
+          <button
+            className="btn2"
+            href="#"
+          >
+            Read more{" "}
+          </button> 
+        </div>
+        <div className="brand-image">
+          <img src="images/Blog 3.png" alt="" className="brand-img" />
+          <button
+            className="btn2"
+            href="#"
+          >
+            Read more{" "}
+          </button> 
+        </div>
+       
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Keepimport React from 'react'
+import 'animate.css';
+
+function Keep() { 
+  return (
+    <section className="section brand">
+    <div className="brand-container container animate__animated animate__backInLeft animate__delay-5s, animate__animate__backOutRight animate__delay-2s">
+      <h2
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex"
+        }}
+      >
+        Keep Up With Us
+      </h2>
+      <div className="brand-images">
+        <div className="brand-image">
+          <img src="images/Blog 1.png" alt="" className="brand-img" />
+          <button
+            className="btn2"
+            href="#"
+          >
+            Read more{" "}
+          </button> 
+        </div>
+        <div className="brand-image">
+          <img src="images/Blog 2.png" alt="" className="brand-img" />
+          <button
+            className="btn2"
+            href="#"
+          >
+            Read more{" "}
+          </button> 
+        </div>
+        <div className="brand-image">
+          <img src="images/Blog 3.png" alt="" className="brand-img" />
+          <button
+            className="btn2"
+            href="#"
+          >
+            Read more{" "}
+          </button> 
+        </div>
+       
       </div>
     </div>
   </section>
